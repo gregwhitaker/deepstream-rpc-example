4d1295b8aae49c1ba4328ee1c35da5051b9fab2b
gregwhitaker <gwhitake@gmail.com>
2018-07-07 09:42:42 -0700

    Initial Commit

---
+++ bar-service/src/main/java/demo/deepstream/service/bar/Main.java
@@ -0,0 +1,8 @@
+package demo.deepstream.service.bar;
+
+public class Main {
+
+    public static void main(String... args) {
+
+    }
+}
---
+++ foo-service/src/main/java/demo/deepstream/service/foo/Main.java
@@ -0,0 +1,8 @@
+package demo.deepstream.service.foo;
+
+public class Main {
+
+    public static void main(String... args) {
+
+    }
+}
