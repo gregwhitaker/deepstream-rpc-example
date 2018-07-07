152504702e2528164b132decb99eae68dc188d37
gregwhitaker <gwhitake@gmail.com>
2018-07-07 10:52:41 -0700

    Doing work

--- bar-service/src/main/java/demo/deepstream/service/bar/Main.java
+++ bar-service/src/main/java/demo/deepstream/service/bar/Main.java
@@ -15,16 +15,60 @@
  */
 package demo.deepstream.service.bar;
 
+import com.google.gson.Gson;
+import io.deepstream.DeepstreamClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
+ * Provides the foo-service to Deepstream.
  */
 public class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String... args) {
+    public static void main(String... args) throws Exception {
+        // Connect to Deepstream
+        DeepstreamClient client = new DeepstreamClient("ws://localhost:6020");
+        client.login();
 
+        // Provide a service named "services.bar"
+        client.rpc.provide("services.bar", (name, data, rpcResponse) -> {
+            Gson gson = new Gson();
+            System.out.println(data);
+            rpcResponse.ack();
+        });
+
+        // Block main thread to prevent JVM exit
+        Thread.currentThread().join();
+    }
+
+    /**
+     *
+     */
+    static class RequestMessage {
+        final String name;
+
+        RequestMessage(final String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    /**
+     *
+     */
+    static class ResponseMessage {
+        final String message;
+
+        ResponseMessage(final String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
     }
 }
